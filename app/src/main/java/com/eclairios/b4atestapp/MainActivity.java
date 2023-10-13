60ea41b24e09cc2773ab8638b0bbecbe79da31aa
waqasofswat <waqasofswat@gmail.com>
2023-10-13 16:51:50 +0500

    b4a integration

---
+++ app/src/main/java/com/eclairios/b4atestapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.eclairios.b4atestapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.back4app_app_id))
+                .clientKey(getString(R.string.back4app_client_key))
+                .server(getString(R.string.back4app_server_url))
+                .build());
+
+        ParseObject firstObject = new  ParseObject("FirstClass");
+        firstObject.put("message","Hey ! First message from android. Parse is now connected");
+        firstObject.saveInBackground(e -> {
+            if (e != null){
+                Log.e("Logggg", e.getLocalizedMessage());
+            }else{
+                Log.d("Logggg","Object saved.");
+            }
+        });
+
+    }
+}
